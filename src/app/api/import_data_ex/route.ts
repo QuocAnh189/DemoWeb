73c220a0e93756af7c3aec1f98e990752eba10c3
QuocAnh189 <anhquoc18092003@gmail.com>
2023-12-05 15:13:22 +0700

    edit

--- src/app/api/import_data_ex/route.ts
+++ src/app/api/import_data_ex/route.ts
@@ -14,7 +14,7 @@ export async function GET() {
         await sql`INSERT INTO ${Table} (_id, avatar, name, description, age, dislike, CreatedAt) VALUES (${uuidv4()},'https://res.cloudinary.com/dfl3qnj7z/image/upload/v1699412607/150019589_1360502140983409_7746736301991057729_n_feem7c.jpg','Phan Ngọc Phước','Bạn này giàu tình cảm nhưng nghèo tiền',3,true,'4/13/2006');`;
         await sql`INSERT INTO ${Table} (_id, avatar, name, description, age, dislike, CreatedAt) VALUES (${uuidv4()},'https://res.cloudinary.com/dfl3qnj7z/image/upload/v1699412621/244051929_2995551894023050_3339734551184003087_n_ursnvq.jpg','Nguyễn Văn Quốc Tuấn','Bạn này rất thích nói đạo lí',3,false,'4/13/2006');`;
         await sql`INSERT INTO ${Table} (_id, avatar, name, description, age, dislike, CreatedAt) VALUES (${uuidv4()},'https://res.cloudinary.com/dfl3qnj7z/image/upload/v1699413453/z9epffilyf4of4d2skql.jpg','Trần Phước Anh Quốc','Hehe , bạn này tuyệt vời thôi rồi !',3,true,'4/13/2006');`;
-        revalidatePath('/page/home');
+        revalidatePath('/home');
         return NextResponse.json({ message: 'Import successfully' }, { status: 200 });
     } catch (error) {
         console.log({ error });
