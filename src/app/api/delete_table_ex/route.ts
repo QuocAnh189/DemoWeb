657374e7bb15e3066cf80cd1b42454cc55630155
QuocAnh189 <anhquoc18092003@gmail.com>
2023-12-10 22:47:28 +0700

    edit

--- src/app/api/delete_table_ex/route.ts
+++ src/app/api/delete_table_ex/route.ts
@@ -4,7 +4,7 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
     try {
-        await sql`DROP TABLE web`;
+        await sql`DROP TABLE nguoiyeucu`;
         revalidatePath('/home');
         return NextResponse.json({ message: 'Delete Successfully' }, { status: 200 });
     } catch (error) {
